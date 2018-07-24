47f132e45b75f6f3a360a80c6bd30c3e164b8521
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-07-24 11:49:46 -0700

    done

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
+
+
+export default (
+    <switch>
+        <Route path='/student/:id' component={Student}/>
+        <Route path='/classlist/:class' component={ClassList}/>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+    </switch>
+)
